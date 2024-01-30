c5871fa8109d02bc424a57ec1c52458ad3c68c2d
shiva167 <108976426+shiva167@users.noreply.github.com>
2024-01-30 23:50:54 +0530

    ready for production

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from "react"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar  from './Component/Navbar';
+import {Home} from './Component/Home';
+import About from './Component/About';
+import NoteState from './Context/Notes/noteState';
+import Alert from './Component/Alert';
+import {Signup} from './Component/Signup';
+import  {Login} from './Component/Login';
 
 function App() {
+  const[alert,setAlert]=useState(null);
+   const showAlert = (message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+   }
+  // const[alert,setAlert]=useState(null);
+  // const showAlert = (message)=>{
+  //  setAlert({
+  //    msg:message
+  //  })
+  //  setTimeout(() => {
+  //    setAlert(null);
+  //  }, 3000);
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <NoteState>
+    <Router>
+    <Navbar/>   
+    <Alert alert={alert}/>
+    <div className="container"> 
+      < Routes>
+          <Route path="/"
+            element={<Home showAlert={showAlert}/>}>
+          </Route>
+          <Route path="/about"
+           element={ <About />}>
+          </Route>
+          <Route path="/login"
+            element={<Login showAlert={showAlert}/>}>
+          </Route>
+          <Route path="/signup"
+           element={ <Signup showAlert={showAlert} />}>
+          </Route>
+    </Routes>
     </div>
+    </Router>
+    </NoteState>
+</>
   );
 }
 
